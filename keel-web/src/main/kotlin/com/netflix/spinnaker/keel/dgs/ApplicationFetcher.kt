eedf1a71861256dcf95b255523de182880f8a9d6
Omar Soriano <osoriano2@gmail.com>
2023-08-25 15:49:02 +0000

    Update environment ordering
    Traverse the graph to get an accurate environment ordering by performing
    a topological sort.
    
    Also, add a unit test to verify the ordering

--- keel-web/src/main/kotlin/com/netflix/spinnaker/keel/dgs/ApplicationFetcher.kt
+++ keel-web/src/main/kotlin/com/netflix/spinnaker/keel/dgs/ApplicationFetcher.kt
@@ -11,7 +11,6 @@ import com.netflix.spinnaker.keel.api.action.ActionType
 import com.netflix.spinnaker.keel.api.actuation.ExecutionSummaryService
 import com.netflix.spinnaker.keel.artifacts.ArtifactVersionLinks
 import com.netflix.spinnaker.keel.auth.AuthorizationSupport
-import com.netflix.spinnaker.keel.core.api.DependsOnConstraint
 import com.netflix.spinnaker.keel.events.EventLevel.ERROR
 import com.netflix.spinnaker.keel.events.EventLevel.WARNING
 import com.netflix.spinnaker.keel.graphql.DgsConstants
@@ -89,15 +88,7 @@ class ApplicationFetcher(
   @DgsData(parentType = DgsConstants.MDAPPLICATION.TYPE_NAME, field = DgsConstants.MDAPPLICATION.Environments)
   fun environments(dfe: DgsDataFetchingEnvironment): List<DataFetcherResult<MdEnvironment>> {
     val config = applicationFetcherSupport.getDeliveryConfigFromContext(dfe)
-    return config.environments.sortedWith { env1, env2 ->
-      when {
-        env1.dependsOn(env2) -> -1
-        env2.dependsOn(env1) -> 1
-        env1.hasDependencies() && !env2.hasDependencies() -> -1
-        env2.hasDependencies() && !env1.hasDependencies() -> 1
-        else -> 0
-      }
-    }.map { env ->
+    return config.environments.sortedByDependencies().map { env ->
       val artifacts = config.artifactsUsedIn(env.name).map { artifact ->
         MdArtifact(
           id = "${env.name}-${artifact.reference}",
@@ -361,9 +352,3 @@ class ApplicationFetcher(
 
 //  add function for putting the resources on the artifactVersion
 }
-
-fun Environment.dependsOn(another: Environment) =
-  constraints.any { it is DependsOnConstraint && it.environment == another.name }
-
-fun Environment.hasDependencies() =
-  constraints.any { it is DependsOnConstraint }
